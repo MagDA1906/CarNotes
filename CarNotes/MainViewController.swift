be0c9d2bac05b0c804bf7a6c8b766c870caa0549
Денис Магильницкий <magilnitsky@gmail.com>
2020-10-20 21:58:37 +0300

    first commit

---
+++ CarNotes/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarNotes
+//
+//  Created by Денис Магильницкий on 20.10.2020.
+//  Copyright © 2020 Денис Магильницкий. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
